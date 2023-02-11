9ad662ae78c6ab184565c450decbaaa460db303e
EmmmPad <2124363741@qq.com>
2023-02-11 21:20:57 +0800

    git push

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,11 @@ export default {
   qq:123456789,
   startWith:';',
   asPassword:false,
-  password:'123456'
+  password:'123456',
+  mysql:{
+    host:"127.0.01",
+    user:"root",
+    password:"123456",
+    database:"bot"
+  }
 }
